02efa3debc09b4817642d4144c636f9ef771da1f
AldaHarjian <aldaharjian@gmail.com>
2022-04-19 17:30:20 +0800

    PI

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.delete('/api/customers/:id', (req, res) => {
 })
 
 // app.listen(3000, () => console.log('Listening to port 3000'))
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.warn(`App listneing on ${PORT}`);
 })
